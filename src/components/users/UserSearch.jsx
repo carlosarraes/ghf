976b4da94793665c9dd89c89475f4e6a576e7f60
carraes <carraeshb@gmail.com>
2022-09-26 00:21:01 -0300

    refactor

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,25 +1,27 @@
 import { useState, useContext } from 'react';
 import GithubContext from '../context/github/GithubContext';
 import AlertContext from '../context/alert/AlertContext';
+import { searchUsers } from '../context/github/GithubActions';
 
 const UserSearch = () => {
   const [text, setText] = useState('');
 
-  const { users, searchUsers, clearUsers } = useContext(GithubContext);
+  const { users, dispatch } = useContext(GithubContext);
   const { setAlert } = useContext(AlertContext);
 
   const handleChange = (e) => setText(e.target.value);
-  const handleSubmit = (e) => {
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
     if (text === '') {
       setAlert('Please enter something', 'error');
     } else {
-      searchUsers(text);
+      dispatch({ type: 'SET_LOADING' });
+      const users = await searchUsers(text);
+      dispatch({ type: 'GET_USERS', payload: users });
       setText('');
     }
   };
-
-  const handleClear = (e) => clearUsers();
 
   return (
     <div className="grid grid-cols-1 xl:grid-cols-4 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8">
@@ -43,7 +45,7 @@ const UserSearch = () => {
       </div>
       {users.length > 0 && (
         <div>
-          <button className="btn btn-ghost btn-large" onClick={handleClear}>
+          <button className="btn btn-ghost btn-large" onClick={() => dispatch({ type: 'CLEAR' })}>
             Clear
           </button>
         </div>
--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -5,15 +5,21 @@ import { Link } from 'react-router-dom';
 import Spinner from '../components/layout/Spinner';
 import GithubContext from '../components/context/github/GithubContext';
 import RepoList from '../components/repos/RepoList';
+import { getUserAndRepos } from '../components/context/github/GithubActions';
 
 const User = () => {
-  const { getUser, user, loading, getUserRepos, repos } = useContext(GithubContext);
+  const { user, loading, repos, dispatch } = useContext(GithubContext);
   const params = useParams();
 
   useEffect(() => {
-    getUser(params.login);
-    getUserRepos(params.login);
-  }, []);
+    dispatch({ type: 'SET_LOADING' });
+    const getUserData = async () => {
+      const userData = await getUserAndRepos(params.login);
+      dispatch({ type: 'GET_USER_AND_REPOS', payload: userData });
+    };
+
+    getUserData();
+  }, [dispatch, params.login]);
 
   const {
     name,
